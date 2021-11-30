98189e2161613568b36021f0b13588d4daa9f91d
Matthieu Jacquot <matthieu.jacquot.tech@protonmail.ch>
2021-11-30 09:53:44 +0100

    fix: node dockerfile

--- apps/svc1/src/main.ts
+++ apps/svc1/src/main.ts
@@ -7,6 +7,7 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import * as r from 'rtrim';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,7 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
+    Logger.log(r('I print without question mark ?????', '?'));
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
 }
